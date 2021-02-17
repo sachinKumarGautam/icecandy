dc3cad6af3077700266df861d41ae1e905dbc58c
Sachin Kumar <sachin.kumar@walmartlabs.com>
2021-02-17 17:00:03 +0530

    about page updated

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,8 +1,28 @@
 import React from "react";
+import "./about.css";
 
 class About extends React.Component {
   render() {
-    return <div>This is about page</div>;
+    const text = `We script, design, direct and produce animation films including
+    Television commercials, Public service  announcements, Promotional
+    content for  TV channels, TV serials and feature length films`;
+    return (
+      <div className="about-page-container">
+        <a href="/">
+          <img className="logo" src={"/IC_TEXT.png"} />
+        </a>
+        <p className="text" dangerouslySetInnerHTML={{ __html: text }}></p>
+        <div className="footer">
+          <div>contact us</div>
+          <a href="mailto:hi@icecandy.in" className="email">
+            hi@icecandy.in
+          </a>
+          <div className="copyright">
+            © icecandy . 2021 . All Rights Reserved
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
